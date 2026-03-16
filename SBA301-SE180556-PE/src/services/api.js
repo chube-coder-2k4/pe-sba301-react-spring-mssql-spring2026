6052335d56bf7e2294cd4e484c0dc0938b55180e
chube-coder-2k4 <qhuyddbmt123@gmail.com>
2026-03-16 11:27:42 +0700

    add frontend: apijs call api with axios

---
+++ SBA301-SE180556-PE/src/services/api.js
@@ -0,0 +1,34 @@
+import axios from "axios";
+const axiosInstance = axios.create({
+    baseURL: "http://localhost:8080",
+    headers: {
+        "Content-Type": "application/json",
+    },
+});
+
+export const getAllCategories = () => {
+    return axiosInstance.get("api/categories");
+};
+
+export const searchShoes = (name = "", category = "", page = 0, size = 5) => {
+    return axiosInstance.get("api/shoes/search", {
+        params: {
+            name: name || undefined,
+            category: category || undefined,
+            page,
+            size,
+        },
+    });
+};
+
+export const getShoeById = (id) => {
+    return axiosInstance.get(`/api/shoes/${id}`);
+};
+
+export const createShoes = (shoeData) => {
+    return axiosInstance.post("/api/shoes", shoeData);
+};
+
+export const deleteShoes = (id) => {
+    return axiosInstance.delete(`/api/shoes/${id}`);
+};
